ea1a87cc24f4d7a9f6cf6ada182eb258170d8503
Mahshid Rezakhani <mrezakh@g.clemson.edu>
2026-05-06 23:50:37 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # first solution
+        #return sorted(s) == sorted(t)
+        # Second solution
+        # if len(s) != len(t):
+        #     return False
+        # count_s = {}
+        # count_t = {}
+        # for ch in s:
+        #     count_s[ch] = count_s.get(ch,0) + 1 
+        # for ch in t:
+        #     count_t[ch] = count_t.get(ch,0) + 1
+        # return count_s == count_t
+        # third 
+        if len(s) != len(t):
+            return False
+        count = {}
+        for ch in s:
+            count[ch] = count.get(ch,0)+1
+        for ch in t:
+            count[ch] = count.get(ch,0)-1
+        for value in count.values():
+            if value != 0:
+                return False
+        return True
+
